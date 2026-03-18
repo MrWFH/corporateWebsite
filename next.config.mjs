fb033dd5af5cd0feafa787b529373ec3c0d1c278
wangfh <wangfh@funshion.com>
2026-03-18 14:04:33 +0800

    配置 GitHub Pages basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
+  basePath: isProd ? "/corporateWebsite" : "",
+  assetPrefix: isProd ? "/corporateWebsite/" : "",
   images: {
     unoptimized: true,
   },
